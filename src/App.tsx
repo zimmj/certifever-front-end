e764de50a9fd93f2f7a2bfa96bef77750ae75a03
Ibrahim-Halil-Kuray <71791007+Ibrahim-Halil-Kuray@users.noreply.github.com>
2023-10-29 10:34:00 +0100

    Update App.tsx
    useStates

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,20 @@ import Difficulty from './components/Difficulty';
 import Questions from './components/Questions';
 
 const App: React.FC = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState<string | null>(null);
+  const [selectedDifficulty, setSelectedDifficulty] = useState<string | null>(
+    null
+  );
+  const [apiUrl, setApiUrl] = useState<string | null>(null);
+
+  const handleLanguageSelect = (language: string) => {
+    setSelectedLanguage(language);
+  };
+
+  const handleDifficultySelect = (difficulty: string) => {
+    setSelectedDifficulty(difficulty);
+  };
+
   return <div className="App"></div>;
 };
 
